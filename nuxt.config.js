bfddda5bd161233a2da01ef75fe5c14aca50a6bb
seyi <sheyi103@outlook.com>
2024-03-23 23:37:29 +0100

    updated the api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -75,7 +75,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://api.bitsintravelsandtours.com/api',
+    // baseURL: 'https://api.bitsintravelsandtours.com/api',
+    baseURL: 'https://api-bitsin.sasoftsolutions.com.ng/api',
     // baseURL: 'http://localhost:8000/api',
   },
   auth: {
